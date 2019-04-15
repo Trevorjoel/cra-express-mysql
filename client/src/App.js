ec7776143b50999da321c5af510e38e33f37f282
Trevorjoel <trevsstuff@hotmail.com>
2019-04-15 12:14:55 +1000

    Include foundation, sass & begin portfolio

--- client/src/App.js
+++ client/src/App.js
@@ -1,71 +1,99 @@
 import React, { Component } from 'react';
 
 import logo from './logo.svg';
-
-import './App.css';
+import logo1 from './images/Sign96x96.png';
+import './App.scss';
 
 class App extends Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
+    state = {
+        response: '',
+        post: '',
+        responseToPost: '',
+        queryBody: ''
+    };
 
-  componentDidMount() {
-    this.callApi()
-        .then(res => this.setState({ response: res.express }))
-        .catch(err => console.log(err));
-  }
 
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
+    componentDidMount() {
+        this.callApi()
+            .then(res => this.setState({ queryBody: res.database1, response: res.express  }))
 
-    if (response.status !== 200) throw Error(body.message);
+            .catch(err => console.log(err))
 
-    return body;
-  };
 
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
+    }
 
-    this.setState({ responseToPost: body });
-  };
+    callApi = async () => {
 
-  render() {
-    return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>SSSS</code> and save to reload.
-            </p>
+        const response = await fetch('/api/hello');
+        const body = await response.json();
+        if (response.status !== 200) throw Error(body.message);
 
-          </header>
-          <p>{this.state.response}</p>
-          <form onSubmit={this.handleSubmit}>
-            <p>
-              <strong>Post to Server:</strong>
-            </p>
-            <input
-                type="text"
-                value={this.state.post}
-                onChange={e => this.setState({ post: e.target.value })}
-            />
-            <button type="submit">Submit</button>
-          </form>
-          <p>{this.state.responseToPost}</p>
-        </div>
-    );
-  }
+        return body;
+    };
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        const response = await fetch('/api/world', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ post: this.state.post }),
+        });
+        const body = await response.text();
+
+        this.setState({ responseToPost: body });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <nav className="top-bar topbar-responsive">
+                    <div className="top-bar-title">
+          <span data-responsive-toggle="topbar-responsive" data-hide-for="medium">
+              { /*  <button className="menu-icon" type="button" data-toggle /> */}
+          </span>
+                        <img src={logo1} />
+                        <a className="topbar-responsive-logo" href="#"><strong>Trevor Joel</strong></a>
+                    </div>
+                    <div id="topbar-responsive" className="topbar-responsive-links">
+                        <div className="top-bar-right">
+                            <ul className="menu simple vertical medium-horizontal">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Projects</a></li>
+                                <li>
+                                    <button type="button" className="button hollow topbar-responsive-button">CONTACT</button>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>SSSS</code> and save to reload.
+                    </p>
+
+                </header>
+                <p>{this.state.queryBody}</p>
+                <p>{this.state.response}</p>
+                <form onSubmit={this.handleSubmit}>
+                    <p>
+                        <strong>Post to Server:</strong>
+                    </p>
+                    <input
+                        type="text"
+                        value={this.state.post}
+                        onChange={e => this.setState({ post: e.target.value })}
+                    />
+                    <button type="submit">Submit</button>
+                </form>
+                <p>{this.state.responseToPost}</p>
+            </div>
+        );
+    }
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let connection = mysql.createConnection({
     host     : '127.0.0.1',
@@ -19,15 +21,36 @@ connection.connect((err) =>{
     console.log('connected....')
 });
 
-connection.query('SELECT * FROM qualification.employee WHERE empDepartmentID = 2', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The results are: ', results);
+
+
+    connection.query('SELECT empFirstName FROM qualification.employee WHERE empDepartmentID = 2',
+        function (error, results, fields) {
+        if (error) throw error;
+
+        console.log('The results are: ', results);
+            app.post('/api/sql', (req, res) => {
+
+                res.send(
+                    {  database1: results,}
+                );
+            });
+            app.get('/api/sql', (req, res) => {
+                res.send({ database1: results,
+                });
+
+            });
 });
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+
 
 app.get('/api/hello', (req, res) => {
-    res.send({ express: 'Hello it is Express vvv' });
+    res.send({ express: 'Hello it is Express back-end',
+    });
+
 });
 
 app.post('/api/world', (req, res) => {
